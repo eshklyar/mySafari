c98f2323102923f57bf5c23ceafd62c60ed71822
eshklyar <eshklyar@gmail.com>
2014-10-01 17:11:43 -0500

    on http

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -14,8 +14,6 @@
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 
-
-
 @end
 
 @implementation ViewController
@@ -26,28 +24,48 @@
 }
 
 
+/*    Loading on start
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSLog(@"url=%@", @"http://www.google.com");
+    return YES;
+}
+
+*/
+
+
 //show spinner on tip left while website is loading
 - (void)webViewDidStartLoad:(UIWebView *)webView {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
 }
 
 //stop spinner
-- (void)webViewDidStopLoad:(UIWebView *)webView {
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
 }
 
 
--(BOOL)textFieldShouldReturn:(UITextField *)textField{
 
-    NSURL *url = [NSURL URLWithString: textField.text];
-    NSURLRequest * urlRequest = [NSURLRequest requestWithURL: url];
-    [self.webView loadRequest:urlRequest];
+//  http apendix
+//  NSURL *urlTextField =[NSURL URLWithString:@"http://" %@, urlTextField.textField];
+
+
+
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+
+    if([textField.text containsString:<#(NSString *)#>]){
+    }
+
+    NSString *urlString = textField.text;
+
+     NSURL *url =[NSURL URLWithString:urlString];
+     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+     [self.webView loadRequest:urlRequest];
+
     return YES;
 }
 
-- (IBAction)onStoppedLoadingButtonPressed:(id)sender {
-    [self.webView stopLoading];
-}
 
 - (IBAction)onBackButtonPressed:(id)sender {
     [self.webView goBack];
@@ -57,14 +75,24 @@
     [self.webView goForward];
 }
 
+- (IBAction)onStoppedLoadingButtonPressed:(id)sender {
+    [self.webView stopLoading];
+}
+- (IBAction)onReloadButtonPressed:(id)sender {
+    [self.webView reload];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-
-
+-(void)enableButtons {
+    self.webView.canGoBack;
+    self.webView.canGoForward;
+    self.webView.loading;
+}
 
 
 @end
