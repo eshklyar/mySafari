b7a4ade81657b359991fe620d09bbb331f3fbf78
eshklyar <eshklyar@gmail.com>
2014-10-01 13:16:08 -0500

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Edik Shklyar on 10/1/14.
+//  Copyright (c) 2014 Edik Shklyar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Edik Shklyar on 10/1/14.
+//  Copyright (c) 2014 Edik Shklyar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
